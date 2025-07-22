d5e1fa4323c29c55ead022c673bed8b88b5f2917
saimadhan00 <saimadhan009@gmail.com>
2025-07-22 11:35:28 +0530

    Add files via upload

---
+++ ApacheTask/src/JavaPackage/Apache.java
@@ -0,0 +1,32 @@
+package JavaPackage;
+import java.io.FileInputStream;
+import java.io.IOException;
+import org.apache.poi.xssf.usermodel.*;
+
+public class Apache {
+    public static void main(String[] args) throws IOException {
+        String excelFilePath = "./Datafiles/TestDataFiles.xlsx";
+        FileInputStream inputstream = new FileInputStream(excelFilePath);
+
+        XSSFWorkbook workbook = new XSSFWorkbook(inputstream);
+        XSSFSheet sheet = workbook.getSheetAt(0);
+
+        // Print details for row 1, 2, 3 (indexes 1, 2, 3)
+        for (int rowIndex = 1; rowIndex <= 3; rowIndex++) {
+            XSSFRow row = sheet.getRow(rowIndex);
+            if (row != null) {
+                System.out.print("Row " + rowIndex + " : ");
+                for (int cellIndex = 0; cellIndex < row.getLastCellNum(); cellIndex++) {
+                    XSSFCell cell = row.getCell(cellIndex);
+                    if (cell != null) {
+                        System.out.print(cell.toString() + "\t");
+                    }
+                }
+                System.out.println();
+            }
+        }
+
+        workbook.close();
+        inputstream.close();
+    }
+}
